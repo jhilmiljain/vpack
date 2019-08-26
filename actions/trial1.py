ab0a2a4ec10d6d1d569960ba8fc561c945b4a501
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-26 17:42:26 +0530

    Update trial1.py

--- actions/trial1.py
+++ actions/trial1.py
@@ -6,5 +6,21 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
       
-      def run(self,id)
+      def run(self,id):
+            try:
+                  x={"ID"=id}
+                  y1=json.dumps(x)
+                  url='https://fakerestapi.azurewebsites.net/api/books/1'
+                  headers={'content-type':'application/json'}
+                  response=requests.post(url,headers=headers,data=y1)
+                  print(response)
+                  y2=response.json()
+                  print(y2)
+            except:
+                  sys.exit(0)
+             
+                  
+                  
+                  
+                  
           
