eb9f8cdc938d1fe3165860b556621a73a0f6025a
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-26 14:49:04 +0530

    Update naction.py

--- actions/naction.py
+++ actions/naction.py
@@ -5,7 +5,7 @@ import requests
  from st2common.runners.base_action import Action	
 
 
- class MyBAction(Action):	
+ class MyVAction(Action):	
          def run(self,id,title,description,page_count,excerpt,publish_date)	
                try:	
                      x={"ID":id,"Title":title,"Description":description,	
