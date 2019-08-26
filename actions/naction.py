a3c8c6408d536825de0b3fe7433ed4e5f1a2bb55
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-26 17:14:03 +0530

    Update naction.py

--- actions/naction.py
+++ actions/naction.py
@@ -5,18 +5,19 @@ import json
 from st2common.runners.base_action import Action
 
 class MyAction(Action):
+	
         def run(self,id,title,description,page_count,excerpt,publish_date):
-                try:
+        	try:
               		x={"ID": id,"Title": title,"Description": description,"PageCount": page_count,"Excerpt": excerpt,"PublishDate": publish_date}
                 	y1=json.dumps(x)
 			header_data={'content-type': 'application/json'}
 			url='https://fakerestapi.azurewebsites.net/api/Books'
-			response=requests.post(url,headers=header_data,data=y1)
+			response=requests.post(url,headers=header_data,x=y1)
 			print(response)
 			y2=response.json()
 			print(y2)
 			
 
-	        except:
+	 	except:
                     	sys.exit(0)
 
