e41f253eab88237431c3cc8472a661e009186390
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-26 17:46:14 +0530

    Update trial1.py

--- actions/trial1.py
+++ actions/trial1.py
@@ -8,7 +8,7 @@ class MyAction(Action):
       
       def run(self,id):
             try:
-                  x={"ID"=id}
+                  x={"ID":id}
                   y1=json.dumps(x)
                   url='https://fakerestapi.azurewebsites.net/api/books/1'
                   headers={'content-type':'application/json'}
