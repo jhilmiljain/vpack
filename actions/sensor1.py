9e3c7779b87fabeb067c767b0c75955eb6624e3f
jhilmiljain <41698967+jhilmiljain@users.noreply.github.com>
2019-08-29 10:52:08 +0530

    Update sensor1.py

--- actions/sensor1.py
+++ actions/sensor1.py
@@ -14,11 +14,11 @@ class hellosensor(Sensor):
 
     def run(self):
         while not self._stop:
-            self._logger.debug('JSensor dispatching trigger...')
+            self._logger.debug('helloSensor dispatching trigger...')
             count = self.sensor_service.get_value('Jaction.count') or 0
             payload = {'message': 'hey', 'count': int(count) + 1}
-            self.sensor_service.dispatch(trigger='pack.event1', payload=payload)
-            self.sensor_service.set_value('Jaction.count', payload['count'])
+            self.sensor_service.dispatch(trigger='vpack123.event1', payload=payload)
+            self.sensor_service.set_value('vpack123.count', payload['count'])
             eventlet.sleep(60)
 
     def cleanup(self):
